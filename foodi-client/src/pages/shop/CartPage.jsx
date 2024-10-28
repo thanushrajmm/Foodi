d4e186c5a4088aacdfca0051bed4c850b4b26ce8
thanushrajmm <thanushrajmm@gmail.com>
2024-10-28 15:28:08 +0530

    updated

--- foodi-client/src/pages/shop/CartPage.jsx
+++ foodi-client/src/pages/shop/CartPage.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import useCart from '../../hooks/useCart'
 import { FaTrash } from "react-icons/fa"
 import Swal from 'sweetalert2'
+import { AuthContext } from '../../contexts/AuthProvider'
 
 const CartPage = () => {
     const [cart, refetch] = useCart();
+    const { user } = useContext(AuthContext);
+    const [cartItems, setcartItems] = useState([]);
+
+    // calculate price
+  const calculatePrice = (item) => {
+    return item.price * item.quantity;
+  };
+
+    //handleIncrease function
+    const handleIncrease = (item) => {
+        // console.log(item._id);
+        fetch(`http://localhost:4000/carts/${item._id}`, {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify({ quantity: item.quantity + 1 }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            const updatedCart = cartItems.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return {
+                  ...cartItem,
+                  quantity: cartItem.quantity + 1,
+                };
+              }
+              return cartItem;
+            });
+            setcartItems(updatedCart);
+            refetch();
+          });
+
+          refetch();
+      };
+
+    //handleDecrease function
+    const handleDecrease = (item) => {
+        // console.log(item._id);
+        if (item.quantity > 1) {
+          fetch(`http://localhost:4000/carts/${item._id}`, {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+            body: JSON.stringify({ quantity: item.quantity - 1 }),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              const updatedCart = cartItems.map((cartItem) => {
+                if (cartItem.id === item.id) {
+                  return {
+                    ...cartItem,
+                    quantity: cartItem.quantity - 1,
+                  };
+                }
+                return cartItem;
+              });
+              setcartItems(updatedCart);
+              refetch();
+            });
+
+            refetch();
+        } else {
+          alert("Item cannot be zero");
+        }
+      };
+
+       // calculate total price
+  const cartSubTotal = cart.reduce((total, item) => {
+    return total + calculatePrice(item);
+  }, 0);
+
+  const orderTotal = cartSubTotal;
 
     //handleDelete btn
     const handleDelete = (item) => {
@@ -16,22 +91,22 @@ const CartPage = () => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-              fetch(`http://localhost:4000/carts/${item._id}`, {
-                method: "DELETE"
-              }).then(res => res.json()).then(data => {
-                if(data.deletedCount > 0){
-                    refetch()
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Your file has been deleted.",
-                        icon: "success"
-                      });
-                }
-              })
+                fetch(`http://localhost:4000/carts/${item._id}`, {
+                    method: "DELETE"
+                }).then(res => res.json()).then(data => {
+                    if (data.deletedCount > 0) {
+                        refetch()
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
             }
-          }); 
+        });
     }
     return (
         <div className='section-container'>
@@ -67,32 +142,37 @@ const CartPage = () => {
                         <tbody>
                             {/* row 1 */}
                             {
-                                cart.map((item, index)=> (
-                                <tr key = {index}>
-                               <td>{index + 1}</td>
-                                <td>
-                                    <div className="flex items-center gap-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle h-12 w-12">
-                                                <img
-                                                    src={item.image}
-                                                    alt="" 
-                                                    />
+                                cart.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle h-12 w-12">
+                                                        <img
+                                                            src={item.image}
+                                                            alt=""
+                                                        />
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="font-medium">
-                                {item.name}
-                                </td>
-                                <td>{item.quantity}</td>
-                                <td>{item.price}</td>
-                                <th>
-                                    <button className="btn btn-ghost text-red btn-xs" onClick={() => handleDelete(item)}>
-                                        <FaTrash/>
-                                        </button>
-                                </th>
-                            </tr>
+                                        </td>
+                                        <td className="font-medium">
+                                            {item.name}
+                                        </td>
+                                        <button className='btn btn-xs' onClick={() => handleDecrease(item)}>-</button>
+                                        <input type="number" 
+                                        value={item.quantity}
+                                        onChange={() => console.log(item.quantity)}
+                                        className='w-10 mx-2 text-center overflow-hidden appearance-none' />
+                                        <button className='btn btn-xs' onClick={() => handleIncrease(item)}>+</button>
+                                        <td>${calculatePrice(item).toFixed(2)}</td>
+                                        <th>
+                                            <button className="btn btn-ghost text-red btn-xs" onClick={() => handleDelete(item)}>
+                                                <FaTrash />
+                                            </button>
+                                        </th>
+                                    </tr>
                                 ))
                             }
                         </tbody>
@@ -100,7 +180,23 @@ const CartPage = () => {
                 </div>
             </div>
 
-            {/*  */}
+            {/* Customer Details */}
+            <div className="my-12 flex flex-col md:flex-row justify-between items-start">
+                <div className="md:w-1/2 space-y-3">
+                    <h3 className="text-lg font-medium">Customer Details</h3>
+                    <p>Name: {user.displayName}</p>
+                    <p>Email: {user.email}</p>
+                    <p>User Id: {user.uid}</p>
+                </div>
+                <div className="md:w-1/2 space-y-3">
+                    <h3 className="text-lg font-medium">Shopping Details</h3>
+                    <p>Total Items: {cart.length}</p>
+                    <p>Total Price: ${orderTotal.toFixed(2)}</p>
+                    <button className="btn bg-green text-white">
+                        Proceed Checkout{" "}
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
--- foodi-server/index.js
+++ foodi-server/index.js
@@ -73,6 +73,24 @@ async function run() {
       res.send(result);
     })
 
+    //update carts quantity
+    app.put("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const { quantity } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: parseInt(quantity, 10),
+        },
+      };
+      const result = await cartCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+    });                                
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
